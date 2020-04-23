3b0adab9a025ff6cc12c03cccfeb75a0907cf8e9
Dmytrii Lysak <dlysak@live.com>
2020-04-23 23:40:43 +0300

    5

---
+++ src/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,30 @@
+import { PipeTransform, BadRequestException } from "@nestjs/common";
+import { TaskStatus } from "src/tasks/task.model";
+
+export class TaskStatusValidationPipe implements PipeTransform {
+    readonly allowedStatuses = [
+        TaskStatus.OPEN,
+        TaskStatus.IN_PROGRESS,
+        TaskStatus.DONE,
+    ]
+
+    transform(value: any) {
+        // tmo fix 500 err
+        if (value === undefined) {
+            throw new BadRequestException(`"${value}" is an invalid status`);
+        }
+
+        value = value.toUpperCase();
+
+        if (!this.isStatusValid(value)) {
+            throw new BadRequestException(`"${value}" is an invalid status`);
+        }
+
+        return value;
+    }
+
+    private isStatusValid(status: any) {
+        const idx = this.allowedStatuses.indexOf(status);
+        return idx !== -1;
+    }
+}
