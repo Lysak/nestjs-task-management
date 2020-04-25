bbfef99240ec754f3363e527fffacfd77db70a4e
Dmytrii Lysak <dlysak@live.com>
2020-04-25 18:00:50 +0300

    clean code

--- src/tasks/pipes/task-status-validation.pipe.ts
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -2,27 +2,27 @@ import { PipeTransform, BadRequestException } from "@nestjs/common";
 import { TaskStatus } from "src/tasks/task.model";
 
 export class TaskStatusValidationPipe implements PipeTransform {
-    readonly allowedStatuses = [
-        Object.values(TaskStatus)
-    ]
+  readonly allowedStatuses = [
+    Object.values(TaskStatus)
+  ]
 
-    transform(value: any) {
-        // tmp fix 500 err
-        if (value === undefined) {
-            throw new BadRequestException(`"${value}" is an invalid status`);
-        }
-
-        value = value.toUpperCase();
-
-        if (!this.isStatusValid(value)) {
-            throw new BadRequestException(`"${value}" is an invalid status`);
-        }
-
-        return value;
+  transform(value: any) {
+    // tmp fix 500 err
+    if (value === undefined) {
+      throw new BadRequestException(`"${value}" is an invalid status`);
     }
 
-    private isStatusValid(status: any) {
-        const idx = this.allowedStatuses.indexOf(status);
-        return idx !== -1;
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`"${value}" is an invalid status`);
     }
+
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    const idx = this.allowedStatuses.indexOf(status);
+    return idx !== -1;
+  }
 }
