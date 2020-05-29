a07a950fe585dd14271fdf0dbfca39905528c428
Dmytrii Lysak <dlysak@live.com>
2020-05-29 22:03:23 +0300

    8.3

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
